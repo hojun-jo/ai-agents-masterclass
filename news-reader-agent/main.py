46501bf42d1b617d35942bdc6a87edd5ffa3e296
Moon <1tjsen@naver.com>
2026-06-08 20:28:31 +0900

    feat: add CrewAI workflow to news reader agent

--- news-reader-agent/main.py
+++ news-reader-agent/main.py
@@ -1,6 +1,55 @@
-def main():
-    print("Hello from news-reader-agnet!")
+import dotenv
 
+dotenv.load_dotenv()
 
-if __name__ == "__main__":
-    main()
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import count_letters
+
+@CrewBase
+class TranslatorCrew:
+  
+  @agent
+  def translator_agent(self):
+    return Agent(
+      config=self.agents_config["translator_agent"],
+    )
+  
+  @agent
+  def counter_agent(self):
+    return Agent(
+      config=self.agents_config["counter_agent"],
+      tools=[count_letters],
+    )
+  
+  @task
+  def translate_task(self):
+    return Task(
+      config=self.tasks_config["translate_task"],
+    )
+  
+  @task
+  def retranslate_task(self):
+    return Task(
+      config=self.tasks_config["retranslate_task"],
+    )
+  
+  @task
+  def count_task(self):
+    return Task(
+      config=self.tasks_config["count_task"],
+    )
+  
+  @crew
+  def assemble_crew(self):
+    return Crew(
+      agent=self.agents,
+      tasks=self.tasks,
+      verbose=True,
+    )
+  
+TranslatorCrew().assemble_crew().kickoff(
+  inputs = {
+    "sentence": "I'm Nico and I like to ride my bicicle in Napoli.",
+  }
+)
